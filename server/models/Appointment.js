e0a103d4232f3f58f5fe66a95ab35fb13f558af4
Marjorie <marjoriebaut.w@gmail.com>
2023-06-13 17:44:26 -0400

    added mutation into resolvers

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,29 +1,24 @@
 const { Schema, model, default: mongoose } = require("mongoose");
 const appointmentSchema = new Schema({
-  appt_date: {
-    type: Date,
-    default: Date.now,
-  },
-  appt_time: {
-    type: Data,
-  },
-
-  //Parisa's suggestion
-  timeSlot: {
-    type: Date,
-    required: true,
-  },
-  selectedDate: {
-    type: Date,
-    required: true,
-  },
-  //End Parisa's suggestion
-//   appoitment ID id the phone number
-    appointmentId: {
+    appt_date: {
+        type: Date,
+        default: Date.now,
+    },
+    appt_time: {
+        type: Data,
+    },
+    //Parisa's suggestion
+    timeSlot: {
+        type: Date,
+        required: true,
+    },
+    //End Parisa's suggestion
+    //   appoitment ID id the phone number
+    referencePhoneNumber: {
         type: String,
         required: true,
     }
-
+// ITHINK WE HAVE TO UPDATE DEF LINE 62 WITH REFERENCEid
 });
 
 // Parisa's suggestion
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,9 +10,30 @@ const resolvers = {
     },
 
     Mutation: {
+        addPatient: async (parent,{PatientInput})=>{
+            const patient = await Patient.create({PatientInput});
+            return patient;
+        },
+        addAppointment:async (parent,{AppointmentInput})=>{
+            const appointment = await Appointment.create({AppointmentInput});
+
+            await Patient.findOneAndUpdate(
+                {phone_number: referenceNumber},
+                {$addToSet:{appointments: appointment._id}}
+            );
+            return appointment;
+        },
+        deleteAppointment:async(parent,{appointmentId})=>{
+            return Appointment.findOneAndDelete({_id:appointmentId})
+        },
+        updateAppointment:async(parent,{appointmentId})=>{
+            return Appointment.findOneAndUpdate(
+                {_id:appointmentId},
+                {$addToSet: {appointments: {AppointmentInput}}}
+                )
 
 
-
+        }
     }
 };
 
