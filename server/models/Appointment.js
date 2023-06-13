17a952aad40ace61abb9bef4f68b4d97ccc7de54
Marjorie <marjoriebaut.w@gmail.com>
2023-06-13 12:37:55 -0400

    appoitment models

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,1 +1,18 @@
-//
+const {Schema, model} = require('mongoose');
+const appointmentSchema = new Schema({
+    appt_date:{
+        type: Date,
+        default: Date.now,
+    },
+    appt_time:{
+        type: Data
+    },
+    patient:[
+        {
+            type:Schema.types.ObjectId,
+            ref:'Patient',
+        }
+    ]
+});
+
+module.exports= Appointment;
