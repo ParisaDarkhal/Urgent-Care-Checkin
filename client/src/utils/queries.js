14ddb686483effa30d45921b5a88861838a5dd98
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-06-15 12:56:41 -0400

    commented out the appt confirmation

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,37 +1,15 @@
-import { gql } from "@apollo/client"
+import { gql } from "@apollo/client";
 
-export const VIEW_APPOINTMENTS = gql `
-    query view_apointments {
-        patient {
-        first_name
-        last_name
-        }
-        appointment {
-        appt_date
-        appt_time
-        id
-        patientId {
-            first_name
-            last_name
-            appointments {
-            appt_date
-            appt_time
-            id
-            patientId {
-                date_of_birth
-                email
-                first_name
-                gender
-                id
-                insurance
-                last_name
-                phone_number
-                reason_for_visit
-            }
-            }
-        }
-        }
-    }
-
-`
-
+// export const VIEW_APPOINTMENTS = gql`
+//   query Query($patientId: ID) {
+//     patient(patientId: $patientId) {
+//       first_name
+//       last_name
+//       appointments {
+//         appt_date
+//         appt_time
+//         id
+//       }
+//     }
+//   }
+// `;
